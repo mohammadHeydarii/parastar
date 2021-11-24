a0056e1b0ac5e18defb512cfb95662b4075c503e
mohammadheydarii <mohammad.heydri7977@gmail.com>
2021-11-25 00:53:02 +0330

    modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./Components/partials/Header";
+import Navigation from "./Components/partials/Navigation";
 function App() {
   return (
     <>
--- src/Components/partials/Header.jsx
+++ src/Components/partials/Header.jsx
@@ -1,45 +1,12 @@
-import { Grid } from "@mui/material";
-
-import Styles from "../../styles/Header.module.css";
-
-import Logo from "../elements/Logo";
-import Buttons from "../elements/Buttons";
+import MobileNavigation from "./MobileNavigation";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <>
-      <nav className={Styles.header}>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
-          <Grid item>
-            <Buttons
-              variant="contained"
-              color="success"
-              size="medium"
-              text="ورود یا ثبت نام"
-            />
-          </Grid>
-          <Grid item>
-            <a>مشاوره روانشناسی</a>
-          </Grid>
-          <Grid item>
-            <a>خدمات پزشکی در منزل</a>
-          </Grid>
-          <Grid item>
-            <a>خدمات پرستاری</a>
-          </Grid>
-          <Grid item>
-            <a>خانه</a>
-          </Grid>
-          <Grid item>
-            <Logo />
-          </Grid>
-        </Grid>
-      </nav>
+      {/* <Navigation /> */}
+
+      <MobileNavigation />
     </>
   );
 };
